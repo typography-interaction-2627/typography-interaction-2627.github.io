337c840c3c4aa84853810c2c12be4aa142bb53c2
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-08-14 12:51:52 -0400

    Add WebC

--- buildawesome.config.js
+++ buildawesome.config.js
@@ -1,8 +1,18 @@
+import webC from '@11ty/eleventy-plugin-webc'
+
 export default (config) => {
+	// Setup.
+	config.addGlobalData('layout', 'base.webc')
+	config.addPlugin(webC, { components: 'templates/inline/**/*.webc' })
+
 	return {
 		dir: {
 			input: 'content',
 			output: '_site',
+
+			// Relative to `input`.
+			layouts: '../templates',
 		},
+		htmlTemplateEngine: 'webc',
 	}
 }
