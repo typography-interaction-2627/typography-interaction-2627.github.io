5ab0a91d8497790806798eba5846aa46d3cb28d2
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-08-22 20:32:43 -0400

    Toss solid “patterns”

--- data/buildawesomeComputed.js
+++ data/buildawesomeComputed.js
@@ -59,7 +59,7 @@ const getH1 = (data) => (data.page.rawInput.match(/^# (.+)/m)?.[1].trim() || dat
 
 export default {
 	date: (data) => getWeek(data)?.date,
-	sequence: data => getSequence(data).findIndex(page => page.inputPath === data.page.inputPath) + 1,
+	sequence: data => getSequence(data).findIndex(page => page.inputPath === data.page.inputPath),
 	title: (data) => inCollection(data, 'weeks')
 		? `Week ${data.page.fileSlug}`
 		: inCollection(data, 'projects')
