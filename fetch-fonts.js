1a70860350f762c2995e9829731b5e707e5c9b94
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-08-26 00:28:44 -0400

    You’ll want to fetch Chicago

--- fetch-fonts.js
+++ fetch-fonts.js
@@ -11,6 +11,7 @@ const FONTS = [
 	{ name: 'geneva.woff2', path: 'exports/web/geneva.woff2' },
 	{ name: 'new-york.woff2', path: 'exports/web/new-york.woff2' },
 	{ name: 'monaco.woff2', path: 'exports/web/monaco.woff2' },
+	{ name: 'chicago.woff2', path: 'exports/web/chicago.woff2' },
 ]
 
 const REF = process.env.FONTS_REF ?? 'main'
