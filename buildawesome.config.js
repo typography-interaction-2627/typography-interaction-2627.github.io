69d7d9ac5c74c6d581fcf81863a181e6272f6bc7
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-09-16 01:15:10 -0400

    TODO

--- buildawesome.config.js
+++ buildawesome.config.js
@@ -143,8 +143,7 @@ export default (config) => {
 
 	// Heading wrapping/links.
 	const markdownAnchors = (markdown) => markdown.use(markdownItAnchor, {
-		// TODO Apostrophes!
-		// TODO Inline overrides don’t work (search `#external`)! It is the `section` wrapping.
+		// TODO Apostrophes! And `&shy;`
 		// TODO Could this be moved to a `.webc` Same for `markdownAsides`?
 		permalink: (slug, opts, state, idx) => {
 			const headingId = state.tokens[idx].attrs.find(([id]) => id === 'id')[1]
